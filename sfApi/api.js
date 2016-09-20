7cc0ae77a1d9bb7b2b25f657ef9c789c5939b9ce
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-09-20 13:10:05 -0600

    add redirect uri to env var

--- sfApi/api.js
+++
@@ -1,5 +0,0 @@
-var conn = require('salesForce');
-
-module.exports = {
-    
-}
--- sfApi/salesForce.js
+++ sfApi/salesForce.js
@@ -5,16 +5,17 @@ var conn = new jsforce.Connection({
     oauth2: {
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        redirectUri: 'https://sforceintegration.herokuapp.com'
+        redirectUri: process.env.REDIRECT_URI
     }
 });
 conn.login(process.env.USERNAME, process.env.PASSWORD, function(err, userInfo) {
     if (err) {
         return console.error(err);
     }
-
 });
 
 
 
 module.exports = conn;
+
+REDIRECT_URI
