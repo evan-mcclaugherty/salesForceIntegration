fdecaf343e9c29d5abb2c1ccd6f476a194fc7a6d
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-09-20 11:57:24 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/salesforce'
-  }
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/salesforce'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
 
 };
